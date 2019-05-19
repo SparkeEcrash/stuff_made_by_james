f8d78a4a8a8c4824c5ce93dcb8aa37657aa9ad48
SparkeEcrash <jpark121989@gmail.com>
2019-05-18 23:57:21 -0700

    test solution

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  mongoURI: 'mongodb://jpark1219:password123@ds133875.mlab.com:33875/myapps-prod',
+  googleClientID: '843919268465-1p7oo85j43ic3sonrtca16ehhckh51pp.apps.googleusercontent.com',
+  googleClientSecret: 'w91qDOyjZ16ywaIBI8qpTPV_'
 }
